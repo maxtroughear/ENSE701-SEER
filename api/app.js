57faf83fe090d38eca8e4ecef4eef2fb7af564ec
Max Troughear <max@maxtroughear.dev>
2020-08-06 12:21:07 +1200

    Use new server discovery and monitoring engine
    Don't use the deprecated engine

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
