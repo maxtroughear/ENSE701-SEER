e8ee66e55718d4b2ee01acfd7038497360824004
Max Troughear <max@maxtroughear.dev>
2020-08-10 17:20:41 +1200

    Add new endpoint for requesting a message from DB

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.json({
-      message: "Hello World",
+router.get('/', function (req, res, next) {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+router.get('/mongo', function (req, res, next) {
+  const db = mongoose.connection;
+
+  db.collection('sample').findOne({}, function(err, doc) {
+    if (err) {
+      res.status(500).end();
+      return;
     }
-  );
+    res.status(200);
+    res.json({
+      message: doc.message,
+    });
+  });
 });
 
 module.exports = router;
