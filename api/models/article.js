4f5fbcf3630b61d21a38391eea4d17c64f322a7f
Max Troughear <max@maxtroughear.dev>
2020-09-28 13:01:58 +1300

    Add very basic article schema

---
+++ api/models/article.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+// the article schema defines the fields an article can have, this will be used when searching the db
+const articleSchema = new Schema({
+  title: String
+});
+
+// the schema must be turned to a model to be used and a collection can be created by mongoose
+const Article = model('Article', articleSchema);
+
+module.exports = {
+  Article
+};
