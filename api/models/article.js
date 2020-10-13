90bd8f5ed0ac50e6058ef6dd339365ccbc9d2625
Max Troughear <max@maxtroughear.dev>
2020-10-13 15:14:33 +1300

    update article schema

--- api/models/article.js
+++ api/models/article.js
@@ -3,7 +3,10 @@ const { Schema, model } = require("mongoose");
 // the article schema defines the fields an article can have, this will be used when searching the db
 const articleSchema = new Schema({
   title: String,
-  date: Date,
+  year: Number,
+  author: String,
+  category: String,
+  summary: String,
 });
 
 // the schema must be turned to a model to be used and a collection can be created by mongoose
